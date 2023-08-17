d2de7894d1f2fca96b1c96fd1b304b8add009427
David Ruano <davidruanoruiz@gmail.com>
2023-08-17 13:55:13 +0200

    Segunda subida del curso

--- workspace/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ workspace/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -5,24 +5,30 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 import jakarta.validation.Valid;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
 		Usuario usuario = new Usuario();
+		usuario.setNombre("John");
+		usuario.setApellido("Dow");
+		usuario.setIdentificador("123.456.789-K");
 		model.addAttribute("titulo", "Formulario usuarios");
 		model.addAttribute("usuario", usuario );
 		return "form";
 	}
 
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
 		model.addAttribute("titulo", "Resultado del formulario");
 
 		if (result.hasErrors()) {
